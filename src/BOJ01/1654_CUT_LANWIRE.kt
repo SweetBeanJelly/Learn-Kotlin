1cc39d55600355091268481a98227d4aed764b1d
SweetBeanJelly <chutedeau@daum.net>
2021-01-25 13:35:34 +0900

    0125

---
+++ src/BOJ01/1654_CUT_LANWIRE.kt
@@ -0,0 +1,28 @@
+package BOJ01
+
+import java.util.*
+
+/*
+ N개의 랜선 길이 만큼 K개의 랜선을 자르기, 만들 수 있는 최대 랜선의 길이 구하기
+*/
+
+fun main() {
+    val input = StringTokenizer(readLine()!!)
+    val K = input.nextToken().toInt()
+    val N = input.nextToken().toInt()
+
+    val cut = IntArray(K)
+    for (i in 0 until K) cut[i] = readLine()!!.toInt()
+    Arrays.sort(cut)
+
+    var start: Long = 1
+    var end = cut[K - 1].toLong()
+
+    while (start <= end) {
+        var cnt: Long = 0
+        val mid = (start + end) / 2
+        for (i in 0 until K) cnt += cut[i] / mid
+        if (cnt < N) end = mid - 1 else if (cnt >= N) start = mid + 1
+    }
+    print(end)
+}
---
+++ src/BOJ01/2805_CUT_TREE.kt
@@ -0,0 +1,36 @@
+package BOJ01
+
+import java.util.*
+
+/*
+ 필요한 M 미터의 나무를 집에 가져가기 위해 나무 절단기에 설정할 수 잇는 높이의 최댓값 구하기
+*/
+
+fun main() = with(Scanner(System.`in`)) {
+    val N = nextInt()
+    val M = nextInt()
+    val tree = IntArray(N)
+
+    var start = 0
+    var end = 1
+    var result = 0
+
+    for (i in tree.indices) {
+        tree[i] = nextInt()
+        start = start.coerceAtLeast(tree[i])
+    }
+
+    while (start >= end) {
+        var sum: Long = 0
+        val pivot = (0L + start + end) / 2
+        for (i in tree.indices) {
+            val len = tree[i] - pivot
+            if (len > 0) sum += len
+        }
+        if (sum >= M) {
+            end = (pivot + 1).toInt()
+            result = result.coerceAtLeast(pivot.toInt())
+        } else start = (pivot - 1).toInt()
+    }
+    println(result)
+}
