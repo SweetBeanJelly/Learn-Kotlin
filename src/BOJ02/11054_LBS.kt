29e731a4ac074e020343812c1e3a57cab5fcd37d
SweetBeanJelly <chutedeau@daum.net>
2021-02-11 22:25:23 +0900

    0211

---
+++ src/BOJ02/11054_LBS.kt
@@ -0,0 +1,24 @@
+package BOJ02
+
+/*
+ 바이토닉 수열이면서 가장 긴 수열의 길이를 구하기
+*/
+
+fun main() {
+    val N = readLine()!!.toInt()
+    val array = readLine()!!.split(" ").map { it.toInt() }
+    val a = IntArray(N) {0}
+    val b = IntArray(N) {0}
+
+    for(i in array.indices) {
+        for(j in 0 until i) if (array[i] > array[j] && a[i] < a[j]) a[i] = a[j]
+        a[i] += 1
+    }
+    for(i in N - 1 downTo 0) {
+        for (j in N - 1 downTo i) if (array[i] > array[j] && b[i] < b[j]) b[i] = b[j]
+        b[i] += 1
+    }
+    val sum = IntArray(N)
+    for(i in sum.indices) sum[i] = a[i] + b[i]
+    print(sum.max()?.minus(1))
+}
