1fa8ab0a9c015cab2a3ae36d19f10166f5057391
SweetBeanJelly <chutedeau@daum.net>
2021-02-22 08:55:09 +0900

    0222

---
+++ src/BOJ02/11653_F_THEOREMOF_A.kt
@@ -0,0 +1,15 @@
+package BOJ02
+
+/*
+ 정수 N이 주어졌을 때, 소인수분해
+*/
+
+fun main() {
+    var (N, M) = readLine()!!.toInt() to 2
+    while (N != 1) {
+        if(N % M == 0) {
+            N /= M
+            println(M)
+        } else M += 1
+    }
+}
