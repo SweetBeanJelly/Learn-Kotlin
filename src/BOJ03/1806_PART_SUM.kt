a35373c6e8c8557c31febe8bb96f2562100498f4
SweetBeanJelly <chutedeau@daum.net>
2021-03-23 21:24:49 +0900

    0323

---
+++ src/BOJ03/1806_PART_SUM.kt
@@ -0,0 +1,37 @@
+package BOJ03
+
+fun main() {
+    val (N, S) = readLine()!!.split(" ").map{ it.toInt() }
+    val num = readLine()!!.split(" ").map{ it.toInt() } + 1
+
+    var (start, end) = 0 to 0
+    var sum = 0
+    var max = Integer.MAX_VALUE
+
+    while(end <= N && start <= N) {
+        if(sum >= S && max > end - start) max = end - start
+        if(sum < S) sum += num[end++]
+        else sum -= num[start++]
+    }
+    println(if (max == Int.Companion.MAX_VALUE) 0 else max)
+}
+
+/*
+    val sc = java.util.Scanner(System.`in`)
+    val (N,S)=IntArray(2){sc.nextInt()}//readLine()!!.split(" ").map{it.toInt()}
+    val A =IntArray(N){sc.nextInt()}// readLine()!!.split(" ").map{it.toInt()}.toTypedArray()
+    var j = 0
+    var sum = 0
+    var ans = N+1
+    for(i in 0 until N) {
+        while(j < N && sum < S) {
+            sum += A[j]
+            j += 1
+        }
+        if(sum >= S) {
+            ans = Math.min(ans, j-i)
+        }
+        sum -= A[i]
+    }
+    println(if(ans > N) 0 else ans)
+*/
