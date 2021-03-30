c088b14177d7253dc1ce56f0c0458934a2c351f4
SweetBeanJelly <chutedeau@daum.net>
2021-03-30 13:42:16 +0900

    0330

---
+++ src/BOJ03/2263_TREE_TRAVERSAL.kt
@@ -0,0 +1,24 @@
+package BOJ03
+
+fun main() {
+    val N = readLine()!!.toInt()
+    val array1 = IntArray(N + 1)
+    val str = readLine()!!.split(" ").map { it.toInt() }
+    for(i in 0 until N) {
+        array1[str[i]] = i
+    }
+    val array2 = readLine()!!.split(" ").map { it.toInt() }
+
+    fun get(a: Int, b:Int, c: Int, d: Int) {
+        if(a > b || c > d) return
+        val root = array2[d]
+        print("$root ")
+        val left = array1[root] - a
+
+        get(a, array1[root] - 1, c, c + left - 1)
+        get(array1[root] + 1, b, c + left, d - 1)
+        return
+    }
+
+    get(0, N - 1, 0, N - 1)
+}
